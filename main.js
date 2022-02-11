a09e01fc0681540d33b2f086d92311904db942d6
Kirill <dremkiri@gmail.com>
2022-02-11 02:17:57 +0200

    1

---
+++ main.js
@@ -0,0 +1,152 @@
+// For 1
+// let K = +prompt('Enter K')
+// let N = +prompt('Enter N (N > 0)')
+// for(let i = K; i <= N; i++){
+//     console.log(K)
+// }
+
+
+// For 2
+// let A = +prompt('Enter first number')
+// let B = +prompt("Enter final number")
+// for(let i = A; i <= B; i++){
+//     console.log(i)
+// }
+
+
+// For 3
+// let A = +prompt('Enter first number (A < B)')
+// let B = +prompt("Enter final number (A < B)")
+// let N = 0
+// for(let i = A; i >= B; i--){
+//     console.log(i)
+//     N++ 
+// }
+// console.log(`Number of numbers is ${N}`)
+
+
+// For 4
+// let summ = +prompt('Enter price')
+// for(let i = 1; i <= 10; i++){
+//     console.log(`Price for ${i}kg is ${summ * i}`)
+// }
+
+
+// For 5
+// let summ = +prompt('Enter price')
+// for(let i = 0.1; i <= 1; i += 0.1){
+//     console.log(`Price for ${i}kg is ${summ * i}`)
+// }
+
+
+// For 6
+// let summ = +prompt('Enter price')
+// for(let i = 1.2; i <= 2; i += 0.2){
+//     console.log(`Price for ${i}kg is ${summ * i}`)
+// }
+
+
+// For 7
+// let A = +prompt('Enter first number')
+// let B = +prompt('Enter last number')
+// let summ = 0
+// for(let i = A; i <= B; i++){
+//     summ += i  
+// }
+// console.log(`Summ of all number is ${summ}`)
+
+
+// For 8
+// let A = +prompt('Enter first number')
+// let B = +prompt('Enter last number')
+// let proizv = 1
+// for(let i = A; i <= B; i++){
+//     proizv *= i  
+// }
+// console.log(`Multiplicatoin of all number is ${proizv}`)
+
+
+// For 9
+// let A = +prompt('Enter first number')
+// let B = +prompt('Enter last number')
+// let kvadr = 0
+// for(let i = A; i <= B; i++){
+//     kvadr += (i ** 2)  
+// }
+// console.log(`Number square of all number is ${summ}`)
+
+
+// For 10
+// let N = +prompt("Enter las number")
+// let summ = 0
+// for(let i = 1; i <= N; i++){
+//     summ += (1 / i)
+// }
+// console.log(`Summ of numbers is ${summ}`)
+
+
+// For 11
+// let N = +prompt("Enter last number")
+// let summ = 0
+// for(let i = 0; i <= N; i++){
+//     summ += ((N + i) ** 2)
+// }
+// console.log(`Summ of numbers is ${summ}`)
+
+
+// For 12
+// let N = +prompt("Enter number of numbers")
+// let proizv = 1
+// let value = 1.1
+// for(let i = 1; i <= N; i++){
+//     proizv *= value 
+//     value += 0.1  
+// }
+// console.log(`Multiplicatoin of numbers is ${proizv}`)
+
+
+// For 13
+// let N = +prompt('Enter your number of numbers');
+// let first = 1.1
+// let second = 1.2
+// let summ = 0
+// let razn = 0
+// for(let i = 1; i <= N; i += 2){
+//     summ += first
+//     first += 0.2 
+// }
+// for(let j = 2; j <= N; j += 2){
+//     razn -= second
+//     second += 0.2 
+// }
+// console.log(`Sum of numbers is ${summ + razn}`);
+
+
+// For 14
+// let N = +prompt('Enter number')
+// let kvadr = 0
+// for(i = 1; i <= N; i++){
+//     kvadr += ((2 * i) - 1)
+//     console.log(`Number square of  number ${i} is ${kvadr}`)
+// }
+
+
+
+// For 15
+// let A = +prompt('Enter your number')
+// let N = +prompt('Enter your degree')
+// let square = 1
+// for(let i = 1; i <= N; i++){
+//     square *= A
+// }
+// console.log(`Degree of number is ${square}`)
+
+
+// For 16
+// let A = +prompt('Enter your number')
+// let N = +prompt('Enter your degree')
+// let square = 1
+// for(let i = 1; i <= N; i++){
+//     square *= A
+//     console.log(`Degree of number ${i} is ${square}`)
+// }
